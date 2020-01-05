a9d2a5006a8404d8b292418b9ffc337064fea933
robbie <robbiejt55@gmail.com>
2020-01-05 17:10:25 +0000

    Added endpoints

--- main.go
+++ main.go
@@ -17,11 +17,15 @@ var posts []POST
 
 func main() {
  router := mux.NewRouter()
-
+ 
  router.HandleFunc("/posts", getPosts).Methods("GET")
  router.HandleFunc("/posts", createPost).Methods("POST")
  router.HandleFunc("/posts/{id}", getPost).Methods("GET")
  router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
  router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
  http.ListenAndServer(":8000", router)
+}
+
+func getPosts(w http.ResponseWriter, r *http.Request){
+	w.Header().
 }
