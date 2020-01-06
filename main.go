0774c9527085f8ab751b133763e0b3c8d36c518d
Robbie <robbiejt55@gmail.com>
2020-01-06 21:07:01 +0000

    Added final methods

--- main.go
+++ main.go
@@ -1,70 +1,52 @@
 package main
 
-import(
-	"github.com/gorilla/mux"
-	"new/http"
+import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"math/rand"
+	"net/http"
 	"strconv"
 )
 
 type Post struct {
-	ID string `json:"id"`
+	ID    string `json:"id"`
 	Title string `json:"title"`
-	Body string `json:"body"`
-
+	Body  string `json:"body"`
 }
 
-var posts []POST
+var posts []Post
 
-func main() {
- router := mux.NewRouter()
- 
- router.HandleFunc("/posts", getPosts).Methods("GET")
- router.HandleFunc("/posts", createPost).Methods("POST")
- router.HandleFunc("/posts/{id}", getPost).Methods("GET")
- router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
- router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
- http.ListenAndServer(":8000", router)
-}
-
-func getPosts(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-type", "application/json")
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(posts)
 }
-
-func getPost(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range posts{
-		if item.ID == params["id"]{
-			json.NewEncoder(w).Encode(item)
-			break
-		}
-		return
-	}
-	json.NewEncoder(w).Encode(&Post{})
-}
-
-
-func createPost(w http.ResponseWriter, r *http.Request){
+func createPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var post Post
-	+ = json.NewDecoder(r.Body).Decode(post)
-	post.ID = strconv.Itoa(rand.Intn(10000000))
+	_ = json.NewDecoder(r.Body).Decode(&post)
+	post.ID = strconv.Itoa(rand.Intn(1000000))
 	posts = append(posts, post)
 	json.NewEncoder(w).Encode(&post)
 }
-
-func updatePost(w http.ResponseWriter, r *http.Request){
+func getPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range posts{
-		if item.ID -- params["id"]{
-			posts = append(posts[:index], posts[index+1]...)
-			
+	for _, item := range posts {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Post{})
+}
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
 			var post Post
-			_ = json.NewDecoder(r.Body,).Decode(post)
+			_ = json.NewDecoder(r.Body).Decode(&post)
 			post.ID = params["id"]
 			posts = append(posts, post)
 			json.NewEncoder(w).Encode(&post)
@@ -73,15 +55,24 @@ func updatePost(w http.ResponseWriter, r *http.Request){
 	}
 	json.NewEncoder(w).Encode(posts)
 }
-
-func deletePost(w htpp.ResponseWriter, r *http.Request){
+func deletePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range posts {
+	for index, item := range posts {
 		if item.ID == params["id"] {
-			posts= append(posts[:index], posts[index+1]...)
+			posts = append(posts[:index], posts[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(posts)
+}
+func main() {
+	router := mux.NewRouter()
+	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	http.ListenAndServe(":8000", router)
 }
